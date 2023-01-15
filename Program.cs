b1fe773127692a0560b71f505fb94c7c1c1aa397
«Kristina» <kristinka007_92@mail.ru>
2023-01-15 16:10:56 +0300

    Задача 27

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
-Console.Write("Введите число A: ");
+/*Console.Write("Введите число A: ");
   int numberA = Convert.ToInt32(Console.ReadLine());
   Console.Write("Введите число B: ");
   int numberB = Convert.ToInt32(Console.ReadLine());
@@ -19,4 +19,34 @@ int Exponentiate(int numberA, int numberB)
 }
 
   int exponentiation = Exponentiate(numberA, numberB);
-  Console.WriteLine("Ответ: " + exponentiation);
+  Console.WriteLine("Ответ: " + exponentiation);*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12  */
+
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberA)
+  {
+    int count = Convert.ToString(numberA).Length;
+    int remainder = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      remainder = numberA - numberA % 10;
+      result = result + (numberA - remainder);
+      numberA = numberA / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberA);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
